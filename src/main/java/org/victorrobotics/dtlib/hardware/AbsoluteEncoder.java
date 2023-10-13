d58f6435e41003135b8df26e69913dc88dcf5f04
MathNerd28 <xander.bhalla@gmail.com>
2023-10-13 13:58:59 -0400

    Add javadoc for DTAbsoluteEncoder and DTAbsoluteEncoderFaults

--- src/main/java/org/victorrobotics/dtlib/hardware/AbsoluteEncoder.java
+++ src/main/java/org/victorrobotics/dtlib/hardware/AbsoluteEncoder.java
@@ -5,29 +5,98 @@ import org.victorrobotics.dtlib.network.DTSendable;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.util.sendable.SendableBuilder;
 
+/**
+ * An interface denoting hardware that supplies an absolutely-referenced
+ * rotational position.
+ */
 public interface AbsoluteEncoder extends DTSendable {
+  /**
+   * @return the underlying vendor implementation of this hardware, used to send
+   *         and receive data.
+   */
   Object getEncoderImpl();
 
+  /**
+   * @return the current position, relative to the configured offset.
+   *
+   * @see #setPosition(Rotation2d)
+   * @see #setZeroPosition(Rotation2d)
+   */
   Rotation2d getPosition();
 
+  /**
+   * @return the current position, absolutely referenced (no offset)
+   *
+   * @see #getPosition()
+   */
   Rotation2d getAbsolutePosition();
 
+  /**
+   * @return the current rotational velocity (per second)
+   */
   Rotation2d getVelocity();
 
+  /**
+   * @return a human-readable string representing the hardware firmware version
+   */
   String getFirmwareVersion();
 
+  /**
+   * @return the faults currently reported by the hardware
+   */
   AbsoluteEncoderFaults getFaults();
 
+  /**
+   * @return true if the hardware is reporting angles that are
+   *         clockwise-positive
+   *
+   * @see #setInverted(boolean)
+   */
   boolean isInverted();
 
+  /**
+   * Determines whether absolute angles will be within the range [0º, 360º) or
+   * [-180º, 180º)
+   *
+   * @param signed
+   *        whether to use a signed output range
+   */
   void setRange(boolean signed);
 
+  /**
+   * Sets the rotational direction that will increase the reported angle.
+   *
+   * @param invert
+   *        true if angles shoulld be clockwise-positive
+   *
+   * @see #isInverted()
+   */
   void setInverted(boolean invert);
 
+  /**
+   * Sets the current hardware position to denote the provided virtual rotation,
+   * applying an offset. This does not apply to absolute measurements.
+   *
+   * @param position
+   *        the new position to set
+   */
   void setPosition(Rotation2d position);
 
+  /**
+   * Sets the given position to an angle of zero. This has no impact on absolute
+   * position.
+   *
+   * @param position
+   *        the position which should be referenced as zero
+   */
   void setZeroPosition(Rotation2d position);
 
+  /**
+   * Sets the current position to an angle of zero. This has no impact on absolute position.
+   *
+   * @see #setPosition(Rotation2d)
+   * @see #setZeroPosition(Rotation2d)
+   */
   void zeroPosition();
 
   @Override
--- src/main/java/org/victorrobotics/dtlib/hardware/AbsoluteEncoderFaults.java
+++ src/main/java/org/victorrobotics/dtlib/hardware/AbsoluteEncoderFaults.java
@@ -1,11 +1,27 @@
 package org.victorrobotics.dtlib.hardware;
 
+/**
+ * An interface representing a fault field for a {@link AbsoluteEncoder}.
+ */
 public interface AbsoluteEncoderFaults {
+  /**
+   * @return true if the device is suffering from a low-voltage type fault.
+   */
   boolean lowVoltage();
 
+  /**
+   * @return true if the device is suffering from a hardware failure.
+   */
   boolean hardwareFailure();
 
+  /**
+   * @return true if the device has declared any faults.
+   */
   boolean hasAnyFault();
 
+  /**
+   * @return true if the device is reporting a fault that doesn't fall into a
+   *         category specified here.
+   */
   boolean other();
 }
